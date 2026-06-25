acff2379d7b987fac03865bb48b4e7f22f982896
simou-rayane <magicsimou@gmail.com>
2026-06-25 02:45:25 +0100

    Add Firebase configuration and initialization code
    This file contains the Firebase configuration and initialization code, including references to various database collections.

---
+++ firebase-config.js
@@ -0,0 +1,27 @@
+// ============ firebase-config.js ============
+// ⚠️ NE JAMAIS COMMITER CE FICHIER SUR UN DÉPÔT PUBLIC !
+// Ce fichier est protégé par .gitignore
+ 
+const firebaseConfig = {
+    apiKey: "AIzaSyAp9akZE9yGLmZAVkcZ7YB4pM1kUP8cJWM",
+  authDomain: "restaurant-menu-9179b.firebaseapp.com",
+  databaseURL: "https://restaurant-menu-9179b-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "restaurant-menu-9179b",
+  storageBucket: "restaurant-menu-9179b.firebasestorage.app",
+  messagingSenderId: "57456460201",
+  appId: "1:57456460201:web:8b40ceeffe81eea918c4b8"
+};
+ 
+// Initialiser Firebase
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+ 
+// Références aux différentes collections
+const productsRef = database.ref('products');
+const ordersRef = database.ref('orders');
+const cartsRef = database.ref('carts');
+const tablesRef = database.ref('tables');
+const categoriesRef = database.ref('categories');
+const restaurantsRef = database.ref('restaurants');
+ 
+console.log('✅ Firebase initialisé');
